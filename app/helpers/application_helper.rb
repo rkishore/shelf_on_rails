3746c648157174eed9d05a0bb94421b59822f7c1
Kishore Ramachandran <keyshawn@gmail.com>
2011-12-16 14:15:28 -0500

    More superficial changes for Demo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,7 +12,7 @@ module ApplicationHelper
 
   # Return a title on a per-page basis
   def title
-    base_title = "DiscountSense alpha demo"
+    base_title = "DiscountSense Demo"
     if @title.nil?
       base_title
     else 
