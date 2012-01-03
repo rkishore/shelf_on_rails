21c21ba91784598f1e6a12bb83c0d4d64e586258
Kishore Ramachandran <keyshawn@gmail.com>
2012-01-03 13:03:12 -0500

    Ruby code for fetching items into database

--- shopstyle-int/code/shopstyle-pull-data-into-db.rb
+++ shopstyle-int/code/shopstyle-pull-data-into-db.rb
@@ -2,16 +2,32 @@
 require 'nokogiri'
 require 'open-uri'
 require 'net/http'
+require 'rubygems'  
+require 'active_record'  
+
+ActiveRecord::Base.establish_connection(  
+                                        :adapter => "sqlite3",  
+                                        :database => "/home/kishore/workspace/djproj/mysite2/sqlite.db"
+                                        )  
+
+class Clothes_brand < ActiveRecord::Base  
+end
+
+class Clothes_item < ActiveRecord::Base  
+end
+
+EXPRESS_BRAND_ID = 1
+JCREW_BRAND_ID = 2
 
 # Pseudo-code
 # 1. Accept cmd-line arguments: brand/store, category
 # 2. Call shopstyle URL with info, and process received arguments
-# 3. Write output to file
+# 3. Write output to db
 
 # Valid categories are: mens-shirts (mens-dress-shirts, mens-longsleeve-shirts, mens-shortsleeve-shirts, mens-polo-shirts, mens-tees-and-tshirts), mens-pants, and mens-jeans
 #                       womens-jeans, womens-sweaters
 
-def process_pernode_info(pernode, fp)
+def process_pernode_info(pernode, category, time)
 
   pr_id = pernode.xpath('//Product/Id')
   pr_name = pernode.xpath('//Product/Name')
@@ -25,28 +41,40 @@ def process_pernode_info(pernode, fp)
   pr_color = pernode.xpath('//Product/Color/Name') 
   pr_size = pernode.xpath('//Product/Size/Name') 
   
-  fp.print(pr_br_name.text, " | ")
-  fp.print(pr_name.text, " | ")
-  
-  i = 0
-  pr_category.each do |l|
-    fp.print(l.text) #.to_s.strip
-    i += 1
-    fp.print(", ") if !(i == pr_category.length)
-  end  
-  
-  fp.print(" | ", pr_price.text)
+  puts time.strftime("%Y-%m-%d %H:%M:%S")
 
-  if ((pr_saleprice.nil? == false) and (pr_saleprice.text.empty? == false))
-    fp.print(", ", pr_saleprice.text, " |\n") 
-  else 
-    fp.print(", ", pr_price.text, " |\n")
+  # Determine brand_id
+  if (pr_br_name.text.strip.casecmp("express") == 0)
+    br_id = EXPRESS_BRAND_ID
+  elsif (pr_br_name.text.strip.casecmp("j.crew") == 0)
+    br_id = JCREW_BRAND_ID
   end
-  #print " ", pr_currency.text, " |\n "
 
-  #print pr_br_name.text, " | ", pr_name.text, " | ", pr_price.text
-  #print ",", pr_saleprice.text if ((pr_saleprice.nil? == false) and (pr_saleprice.text.empty? == false))
-  #print " ", pr_currency.text, " | ", pr_instock.text, " | "
+  # Determine gender
+  spl_cat = category.split("-")
+  if (spl_cat[0].casecmp("womens") == 0)
+    p_gender = 'F'
+  elsif (spl_cat[0].casecmp("mens") == 0)
+    p_gender = 'M'
+  end
+
+  # Determine product categories
+  i = 0
+  p_cat = ['Empty', 'Empty', 'Empty', 'Empty', 'Empty']
+  pr_category.each do |l|
+    p_cat[i] = l.text
+    i += 1
+    break if (i > 4)
+  end  
+
+  # Determine saleprice
+  if ((pr_saleprice.nil? == false) and (pr_saleprice.text.empty? == false))
+    p_saleprice = pr_saleprice.text
+  else 
+    p_saleprice = pr_price.text
+  end
+
+  Clothes_item.create(:brand_id => br_id, :name => pr_name.text, :gender => p_gender, :cat1 => p_cat[0], :cat2 => p_cat[1], :cat3 => p_cat[2], :cat4 => p_cat[3], :cat5 => p_cat[4], :price => pr_price.text, :saleprice => p_saleprice, :insert_date => time.strftime("%Y-%m-%d %H:%M:%S"))
   
   # Print available colors
   #i = 0
@@ -71,10 +99,6 @@ end
 
 def parse_each_product(filename, brand, category, time)
 
-  # Create file to store XML data
-  data_filename = "../data/" + brand.downcase + "-" + category + "-ss-" + time.year.to_s + "-" + time.month.to_s + "-" + time.day.to_s + ".data"
-  fp = File.open(data_filename, 'w')
-
   reader = Nokogiri::XML::Reader.from_io(File.open(filename))
 
   reader.each do |node|
@@ -82,7 +106,7 @@ def parse_each_product(filename, brand, category, time)
     if node.name == 'Product' and node.node_type == Nokogiri::XML::Reader::TYPE_ELEMENT
 
       doc = Nokogiri::XML(node.outer_xml)
-      process_pernode_info(doc, fp)
+      process_pernode_info(doc, category, time)
 
     end
   end
@@ -116,16 +140,22 @@ def get_xml_data(brand, category, time)
   product_cnt_tag = @doc.xpath('//TotalCount')
   product_cnt = product_cnt_tag.text.to_i
 
+  print "Total product count: " + product_cnt.to_s + "\n"
+  #product_cnt_tmp = 5
+
   # Next, we fetch item info 250 items at a time (max. allowed pull number by shopstyle API)
   max_allowed_records = 250 # dictated by shopstyle.com API
   num_iter = product_cnt / max_allowed_records
   num_last_cnt = product_cnt % max_allowed_records
+  print "Num iterations: "+num_iter.to_s+" "+num_last_cnt.to_s+"\n"
   min_cnt = 0
 
-  print "Num iterations: "+num_iter.to_s+" "+num_last_cnt.to_s+"\n"
+  # if product_cnt > max_allowed_records
   if (num_iter > 0)
-    i = 0  
+    print "Should not be here 1 \n"
+    i = 0
     while(i < num_iter)
+      print "Should not be here 2 \n"
       #print i.to_s+" "+min_cnt.to_s+" "+max_allowed_records.to_s+"\n"
       url_str = construct_shopstyle_url(brand, url_cat, min_cnt, max_allowed_records)
       # Dump XML into file
@@ -134,12 +164,13 @@ def get_xml_data(brand, category, time)
       i += 1  
     end
   end
-  
+
   # Fetch remaining item info
   url_str = construct_shopstyle_url(brand, url_cat, min_cnt, num_last_cnt)
+  
   # Dump XML into file
   fetch_xml_into_file(url_str, fp)
-  
+
   return fp
   
 end
@@ -156,17 +187,20 @@ end
 
 if __FILE__ == $0
 
-  if ARGV.length < 2
-    puts "Usage    : ruby $0 brand category"
-    puts "Example 1: ruby $0 express mens-jeans"
-    puts "Example 2: ruby $0 jcrew womens-jeans"
+  if ARGV.length < 3
+    puts "Usage    : ruby $0 brand category /path/to/db"
+    puts "Example 1: ruby $0 express mens-jeans /home/kishore/workspace/djproj/mysite2/sqlite.db"
+    puts "Example 2: ruby $0 jcrew womens-jeans /home/kishore/workspace/djproj/mysite2/sqlite.db"
     exit
   end
 
   store_name = ARGV[0]
   category = ARGV[1]
+  dbpath = ARGV[2]
 
-  puts "Brand: " + store_name + ", Category: " + category
+  puts "Brand: " + store_name + ", Category: " + category + ", dbpath: " + dbpath
+
+  puts "TODO: Open db connection here and pass objects to function below"
 
   time = Time.new  
   xml_fname = get_xml_data(store_name, category, time)  
