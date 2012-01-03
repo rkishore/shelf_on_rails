2683446c021caab3cb3446a2be88ffabfe9c6cf1
Kishore Ramachandran <keyshawn@gmail.com>
2012-01-03 13:18:23 -0500

    Models for Items and Brands

--- tutorial/polls/models.py
+++ tutorial/polls/models.py
@@ -85,3 +85,48 @@ class Promoinfo(models.Model):
 
     def __unicode__(self):
         return self.store + " " + str(self.d) + " " + str(self.promo_type)  
+
+
+class Brands(models.Model):
+    name = models.CharField(max_length=200, default='Nil')
+
+    def __unicode__(self):
+        return self.name
+
+class Items(models.Model):
+    
+    GENDER_CHOICES = (
+        ('M', 'MALE'),
+        ('F', 'FEMALE'),
+        ('A', 'ALL'),
+        )
+    
+    # These choices should be gender-specific                                                                                                                                  
+    #MCAT_CHOICES = (
+    #    ('Nil', 'Empty'),
+    #    ('mens-dress-shirts', 'Dress-shirts'),
+    #    ('mens-tees-and-tshirts', 'T-shirts'),
+    #    ('mens-polo-shirts', 'Polo-shirts'),
+    #    ('mens-shirts', 'Shirts'),
+    #    ('mens-longsleeve-shirts', 'Longsleeve'),
+    #    ('mens-shortsleeve-shirts', 'Shortsleeve'),
+    #    ('mens-jackets', 'Jackets'),
+    #    )
+
+    # Each item is related to a single brand in the forward direction and                                                                                                      
+    # each brand is related to many items in the reverse direction                                                                                                              
+    brand = models.ForeignKey(Brands, default='Nil')
+    gender = models.CharField(max_length=6, choices=GENDER_CHOICES, default='A')
+    cat1 = models.CharField(max_length=100, default='Nil')
+    cat2 = models.CharField(max_length=100, default='Nil')
+    cat3 = models.CharField(max_length=100, default='Nil')
+    cat4 = models.CharField(max_length=100, default='Nil')
+    cat5 = models.CharField(max_length=100, default='Nil')
+    name = models.CharField(max_length=200, default='Nil')
+    price = models.CharField(max_length=10, default='20.00')
+    saleprice = models.CharField(max_length=10, default='10.00')
+    insert_date = models.DateTimeField('Date inserted', default=datetime.now)
+    #available_sizes = models.CharField(max_length=2, choices=SIZE_CHOICES)                                                                                                    
+    #available_colors = models.CharField(max_length=2, choices=COLOR_CHOICES)                                                                                                   
+    def __unicode__(self):
+        return self.name
