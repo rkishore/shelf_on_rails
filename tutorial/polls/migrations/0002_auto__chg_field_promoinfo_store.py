52025981f6881ea06c4ff01c464d03613686891e
Kishore Ramachandran <keyshawn@gmail.com>
2012-01-03 16:37:05 -0500

    Back to preferred schema without data

---
+++ tutorial/polls/migrations/0002_auto__chg_field_promoinfo_store.py
@@ -0,0 +1,83 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Renaming column for 'Promoinfo.store' to match new field type.
+        db.rename_column('polls_promoinfo', 'store', 'store_id')
+        # Changing field 'Promoinfo.store'
+        db.alter_column('polls_promoinfo', 'store_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['polls.Brands']))
+
+        # Adding index on 'Promoinfo', fields ['store']
+        db.create_index('polls_promoinfo', ['store_id'])
+
+
+    def backwards(self, orm):
+        
+        # Removing index on 'Promoinfo', fields ['store']
+        db.delete_index('polls_promoinfo', ['store_id'])
+
+        # Renaming column for 'Promoinfo.store' to match new field type.
+        db.rename_column('polls_promoinfo', 'store_id', 'store')
+        # Changing field 'Promoinfo.store'
+        db.alter_column('polls_promoinfo', 'store', self.gf('django.db.models.fields.CharField')(max_length=20))
+
+
+    models = {
+        'polls.brands': {
+            'Meta': {'object_name': 'Brands'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'default': "'Nil'", 'max_length': '200'})
+        },
+        'polls.choice': {
+            'Meta': {'object_name': 'Choice'},
+            'choice': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'poll': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['polls.Poll']"}),
+            'votes': ('django.db.models.fields.IntegerField', [], {})
+        },
+        'polls.items': {
+            'Meta': {'object_name': 'Items'},
+            'brand': ('django.db.models.fields.related.ForeignKey', [], {'default': "'Nil'", 'to': "orm['polls.Brands']"}),
+            'cat1': ('django.db.models.fields.CharField', [], {'default': "'Nil'", 'max_length': '100'}),
+            'cat2': ('django.db.models.fields.CharField', [], {'default': "'Nil'", 'max_length': '100'}),
+            'cat3': ('django.db.models.fields.CharField', [], {'default': "'Nil'", 'max_length': '100'}),
+            'cat4': ('django.db.models.fields.CharField', [], {'default': "'Nil'", 'max_length': '100'}),
+            'cat5': ('django.db.models.fields.CharField', [], {'default': "'Nil'", 'max_length': '100'}),
+            'gender': ('django.db.models.fields.CharField', [], {'default': "'A'", 'max_length': '6'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'insert_date': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'name': ('django.db.models.fields.CharField', [], {'default': "'Nil'", 'max_length': '200'}),
+            'price': ('django.db.models.fields.CharField', [], {'default': "'20.00'", 'max_length': '10'}),
+            'saleprice': ('django.db.models.fields.CharField', [], {'default': "'10.00'", 'max_length': '10'})
+        },
+        'polls.poll': {
+            'Meta': {'object_name': 'Poll'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'pub_date': ('django.db.models.fields.DateTimeField', [], {}),
+            'question': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+        },
+        'polls.promoinfo': {
+            'Meta': {'unique_together': "(('store', 'd', 'promo_type', 'promo_disc_perc', 'promo_disc_amount', 'promo_disc_lower_bound', 'sex_category', 'item_category'),)", 'object_name': 'Promoinfo'},
+            'code': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
+            'd': ('django.db.models.fields.DateField', [], {}),
+            'free_shipping_lower_bound': ('django.db.models.fields.IntegerField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'item_category': ('django.db.models.fields.IntegerField', [], {}),
+            'promo_disc_amount': ('django.db.models.fields.IntegerField', [], {}),
+            'promo_disc_lower_bound': ('django.db.models.fields.IntegerField', [], {}),
+            'promo_disc_perc': ('django.db.models.fields.IntegerField', [], {}),
+            'promo_type': ('django.db.models.fields.IntegerField', [], {}),
+            'sex_category': ('django.db.models.fields.IntegerField', [], {}),
+            'store': ('django.db.models.fields.related.ForeignKey', [], {'default': "'Nil'", 'to': "orm['polls.Brands']"}),
+            'validity': ('django.db.models.fields.IntegerField', [], {}),
+            'where_avail': ('django.db.models.fields.IntegerField', [], {})
+        }
+    }
+
+    complete_apps = ['polls']
